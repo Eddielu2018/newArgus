51b9acca20de254c69cbd05d7b94b30af73037cb
sun <770345210@qq.com>
2017-11-03 11:34:42 +0800

    用户行为

--- argus/src/main/java/cn/htd/argus/util/DateTimeUtil.java
+++ argus/src/main/java/cn/htd/argus/util/DateTimeUtil.java
@@ -91,6 +91,26 @@ public class DateTimeUtil
         SimpleDateFormat lastDf = new SimpleDateFormat("yyyyMM");
         return lastDf.format(gerCal.getTime()).toString();
     }
+    /**
+     *
+     *
+     * @Title: getLastMonthChar6
+     * @Description: 获取上月信息 以yyyymm格式返回
+     * @Param: @return
+     * @Return: String
+     * @Comment:
+     * @Author: Administrator
+     * @CreateDate: 2012-6-9 下午03:37:12
+     */
+    public static String getLastMonthChar7(String custTime)
+    {
+
+        GregorianCalendar gerCal = new GregorianCalendar();
+        gerCal.setTime(new Date());
+        gerCal.add(GregorianCalendar.MONTH, -1);// 在日期上减一个月
+        SimpleDateFormat lastDf = new SimpleDateFormat("yyyyMM");
+        return lastDf.format(gerCal.getTime()).toString();
+    }
 
     /**
      * 
@@ -355,7 +375,7 @@ public class DateTimeUtil
      * 
      * @Title: getDayChar14ByMunites
      * @Description: 根据分钟加减当前日期
-     * @param days
+     * @param mni
      * @return
      * @return String 返回类型
      */
@@ -618,8 +638,7 @@ public class DateTimeUtil
     /**
      * @Title: getOracleDate
      * @Description: 将指定Date类型参数转换为指定的Oracle日期时间格式字符串
-     * @param inputDate
-     *            传入Date类型参数
+     * @param inputDateTime 传入Date类型参数
      * @return String
      */
     public static String getOracleDate(Date inputDateTime) throws NullPointerException
@@ -2030,7 +2049,6 @@ public class DateTimeUtil
      * 
      * @Title: getSixDayDate
      * @Description: 获取1月1号到传入时间的7个日期节点
-     * @param days
      * @param dataTime
      * @return
      * @return List<String> 返回类型
