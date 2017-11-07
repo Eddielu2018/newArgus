f8ccb479c7556b81c12ee17831dcd143175a1eb4
sun <770345210@qq.com>
2017-11-07 15:51:20 +0800

    用户行为

--- argus/src/main/java/cn/htd/argus/bean/HtyFctUserBeHaviorSearchDTO.java
+++ argus/src/main/java/cn/htd/argus/bean/HtyFctUserBeHaviorSearchDTO.java
@@ -4,37 +4,10 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 public class HtyFctUserBeHaviorSearchDTO {
-	private String endTime;// 日历2
-	private String period;// 时间段
-	private String startTime;// 日历1
-	private String countryName;// 地理范围 国家
-    private String provinceName;// 省
-    private String cityName;// 城市
-    private  String custCountryName;//区
-    private String townName;
-    private String plCode;
-    private String plName;
-    private String ppCode;
-    private String ppName;
-    private String prodCode;
-    private String prodName;
-    private BigDecimal xsAmt;
-    private BigDecimal xsQty;
+    private String endTime;// 日历2
+    private String startTime;// 日历1
+    private String userId;
 
-    public String getCustCountryName() {
-        return custCountryName;
-    }
-
-    public void setCustCountryName(String custCountryName) {
-        this.custCountryName = custCountryName;
-    }
-
-    public void setTownName(String townName) {
-        this.townName = townName;
-    }
-    public String getTownName() {
-        return townName;
-    }
     public String getEndTime() {
         return endTime;
     }
@@ -51,98 +24,11 @@ public class HtyFctUserBeHaviorSearchDTO {
         this.startTime = startTime;
     }
 
-    public String getCountryName() {
-        return countryName;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
-    }
-
-    public String getProvinceName() {
-        return provinceName;
-    }
-
-    public void setProvinceName(String provinceName) {
-        this.provinceName = provinceName;
-    }
-
-    public String getCityName() {
-        return cityName;
-    }
-
-    public void setCityName(String cityName) {
-        this.cityName = cityName;
-    }
-    public String getPlCode() {
-        return plCode;
-    }
-
-    public void setPlCode(String plCode) {
-        this.plCode = plCode;
-    }
-
-    public String getPlName() {
-        return plName;
-    }
-
-    public void setPlName(String plName) {
-        this.plName = plName;
-    }
-
-    public String getPpCode() {
-        return ppCode;
-    }
-
-    public void setPpCode(String ppCode) {
-        this.ppCode = ppCode;
-    }
-
-    public String getPpName() {
-        return ppName;
-    }
-
-    public void setPpName(String ppName) {
-        this.ppName = ppName;
-    }
-
-    public String getProdCode() {
-        return prodCode;
-    }
-
-    public void setProdCode(String prodCode) {
-        this.prodCode = prodCode;
-    }
-
-    public String getProdName() {
-        return prodName;
-    }
-
-    public void setProdName(String prodName) {
-        this.prodName = prodName;
-    }
-
-    public BigDecimal getXsAmt() {
-        return xsAmt;
-    }
-
-    public void setXsAmt(BigDecimal xsAmt) {
-        this.xsAmt = xsAmt;
-    }
-
-    public BigDecimal getXsQty() {
-        return xsQty;
-    }
-
-    public void setXsQty(BigDecimal xsQty) {
-        this.xsQty = xsQty;
-    }
-
-    public String getPeriod() {
-        return period;
-    }
-
-    public void setPeriod(String period) {
-        this.period = period;
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 }
