93a87dab483456b98c32097039a0e05d27a44385
sun <770345210@qq.com>
2017-11-07 16:01:34 +0800

    用户行为

--- argus/src/main/java/cn/htd/argus/dao/HtyFctCustAllDTOMapper.java
+++ argus/src/main/java/cn/htd/argus/dao/HtyFctCustAllDTOMapper.java
@@ -4,28 +4,29 @@ import java.util.List;
 
 import cn.htd.argus.bean.HtyFctCustAnalysisInDTO;
 import cn.htd.argus.bean.HtyFctCustInDto;
+import cn.htd.argus.bean.HtyFctUserBeHaviorSearchDTO;
 import cn.htd.argus.dto.HtyFctCustAllDto;
 
 public interface HtyFctCustAllDTOMapper {
 
-	HtyFctCustAllDto selectByPrimaryKey(Long id);
+    HtyFctCustAllDto selectByPrimaryKey(Long id);
     List<HtyFctCustAllDto> select(HtyFctCustAllDto model);
     List<HtyFctCustAllDto> selectByNoPair(HtyFctCustInDto inDto);
 
     Integer selectForAmtAll(HtyFctCustAnalysisInDTO inDto);
-    
+
     HtyFctCustAllDto selectForAnalysis(HtyFctCustAnalysisInDTO inDto);
-    
+
     Integer selectForAmtOnline(HtyFctCustAnalysisInDTO inDto);
-    
+
     Integer selectForQtyB2b(HtyFctCustAnalysisInDTO inDto);
-    
+
     Integer selectForQtyBoss(HtyFctCustAnalysisInDTO inDto);
-    
+
     Integer selectForQtyHzg(HtyFctCustAnalysisInDTO inDto);
-    
+
     Integer selectForAmtDk(HtyFctCustAnalysisInDTO inDto);
-    
+
     List<HtyFctCustAllDto> selectForPair(HtyFctCustAnalysisInDTO inDto);
 
     List<HtyFctCustAllDto> selectByTimePair(HtyFctCustInDto inDto);
@@ -35,4 +36,6 @@ public interface HtyFctCustAllDTOMapper {
     List<HtyFctCustAllDto> selectByTimeDimension(HtyFctCustInDto inDto);
     //导出明细
     List<HtyFctCustAllDto>queryExport(HtyFctCustAllDto htyFctCustAllDto);
+
+    HtyFctCustAllDto  queryCityAndProByUserId(HtyFctUserBeHaviorSearchDTO htyFctUserBeHaviorSearchDTO);
 }
