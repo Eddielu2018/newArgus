43b8baf6633562c33267d545a12315d7b0d2b1af
sun <770345210@qq.com>
2017-11-07 16:02:40 +0800

    用户行为

--- argus/src/main/java/cn/htd/argus/service/HtyFctCustAllDTOService.java
+++ argus/src/main/java/cn/htd/argus/service/HtyFctCustAllDTOService.java
@@ -3,6 +3,7 @@ package cn.htd.argus.service;
 import java.util.List;
 
 import cn.htd.argus.bean.HtyFctCustInDto;
+import cn.htd.argus.bean.HtyFctUserBeHaviorSearchDTO;
 import cn.htd.argus.dto.HtyFctCustAllDto;
 
 /**
@@ -10,26 +11,26 @@ import cn.htd.argus.dto.HtyFctCustAllDto;
  */
 public interface HtyFctCustAllDTOService {
 
-	HtyFctCustAllDto selectByPrimaryKey(Long id);
+    HtyFctCustAllDto selectByPrimaryKey(Long id);
 
     HtyFctCustAllDto select(HtyFctCustAllDto dto);
 
     List<HtyFctCustAllDto> selectByNoPair(HtyFctCustInDto inDto);
-    
+
     Integer selectForAmtAll(String userId,String startTime,String endTime,int type,Integer regionOrg);
-    
+
     HtyFctCustAllDto selectForAnalysis(String userId,String startTime,String endTime,int type);
-    
+
     Integer selectForAmtOnline(String userId,String startTime,String endTime,int type,Integer regionOrg);
-    
+
     Integer selectForQtyB2b(String userId,String startTime,String endTime,int type,Integer regionOrg);
-    
+
     Integer selectForQtyBoss(String userId,String startTime,String endTime,int type,Integer regionOrg);
-    
+
     Integer selectForQtyHzg(String userId,String startTime,String endTime,int type,Integer regionOrg);
-    
+
     Integer selectForAmtDk(String userId,String startTime,String endTime,int type,Integer regionOrg);
-    
+
     List<HtyFctCustAllDto> selectForPair(String userId,String startTime,String endTime,int type);
 
 
@@ -37,4 +38,5 @@ public interface HtyFctCustAllDTOService {
 
     List<HtyFctCustAllDto>queryExport(HtyFctCustAllDto htyFctCustAllDto);
 
+    HtyFctCustAllDto  queryCityAndProByUserId(HtyFctUserBeHaviorSearchDTO htyFctUserBeHaviorSearchDTO);
 }
